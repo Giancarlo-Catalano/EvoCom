3aad87de7c77beb80f6a75b30ba14a49929dc4e4
Gian <gac00056@students.stir.ac.uk>
2022-09-22 01:17:25 +0100

    Removed useless tests in main

--- main.cpp
+++ main.cpp
@@ -16,74 +16,9 @@ int main() {
        *
      */
 
-#if 0 //checking BlockReport
-    Block block;
-    for (int i=0;i<(1ULL<<12);i++)
-        block.push_back((i*i)%256);
-
-    LOG("The block is ", containerToString(block, 10));
-    GC::BlockReport br(block);
-    LOG(br.to_string());
-#endif
-
-
-#if 0 //checking FileBitWriter
-    std::string fileName = "/home/gian/CLionProjects/Diss_SimplePrototype/SampleFiles/loremIpsum.txt";
-    std::ofstream outStream(fileName);
-    GC::FileBitWriter bw(outStream);
-
-
-    for (size_t i=0;i<2;i++) {
-        //* in ascii is 42 = 00101010
-        const std::array<bool, 8> bits = {0, 0, 1, 1, 1, 0, 1, 0};
-        for (auto bit: bits) bw.pushBit(bit);
-    }
-
-    bw.forceLast();
-
-    //bw.forceLast();
-    outStream.close();
-    dumpFile(fileName);
-#endif
-
-
-#if 0 //checking Huffman Compression
-    std::string fileName = "/home/gian/CLionProjects/Diss_SimplePrototype/SampleFiles/loremIpsum.txt";
-    Block block;
-    for (size_t i=0;i<(1<<12);i++)
-        block.push_back(i/((i>>4)+1));
-
-    LOG("The block to be encoded is ", containerToString(block));
-    LOG("it has the following properties:", GC::BlockReport(block).to_string());
-    GC::RunLengthCompression hc;
-    LOG("The compression to be applied is ", hc.to_string());
-
-    std::ofstream outStream(fileName);
-    GC::FileBitWriter writer(outStream);
-    hc.compress(block, writer);
-    writer.forceLast();
-    outStream.close();
-    dumpFile(fileName);
-
-
-
-    LOG("and then we decompress");
-    std::ifstream inStream(fileName);
-    GC::FileBitReader reader(inStream);
-    Block revertedBlock = hc.decompress(reader);
-
-
-    LOG("The reverted block is", containerToString(revertedBlock));
-    LOG("The original was   ..", containerToString(block));
-#endif
-
-
-#if 1 //checking compression
     std::string fileToBeCompressed = "/home/gian/CLionProjects/Diss_SimplePrototype/SampleFiles/fencingLogoSimple.png";
     std::string compressedFile = "/home/gian/CLionProjects/Diss_SimplePrototype/SampleFiles/compressed.gac";
     std::string decompressedFile = "/home/gian/CLionProjects/Diss_SimplePrototype/SampleFiles/uncompressed.png";
-
-    //LOG("The compressor has the following codes:", GC::SimpleCompressor().to_string());
 
     GC::SimpleCompressor::compress(fileToBeCompressed, compressedFile);
     LOG("The compressedFile has size ", getFileSize(compressedFile));
@@ -91,10 +26,6 @@ int main() {
     GC::SimpleCompressor::decompress(compressedFile, decompressedFile);
 
     LOG("In short: ", getFileSize(fileToBeCompressed), "bytes -> ", getFileSize(compressedFile), "bytes");
-
-
-
-#endif
 
     return 0;
 }
