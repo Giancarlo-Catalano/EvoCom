3ad5bce909aa84f49093ea46eb71d349a9cecd4e
Gian <gac00056@students.stir.ac.uk>
2022-09-25 15:40:03 +0100

    Added a Breeder class, which is used to mutate and crossover individuals

---
+++ Breeder/Breeder.cpp
@@ -0,0 +1,8 @@
+//
+// Created by gian on 25/09/22.
+//
+
+#include "Breeder.hpp"
+
+namespace GC {
+} // GC
---
+++ Breeder/Breeder.hpp
@@ -0,0 +1,16 @@
+//
+// Created by gian on 25/09/22.
+//
+
+#ifndef DISS_SIMPLEPROTOTYPE_BREEDER_HPP
+#define DISS_SIMPLEPROTOTYPE_BREEDER_HPP
+
+namespace GC {
+
+    class Breeder {
+
+    };
+
+} // GC
+
+#endif //DISS_SIMPLEPROTOTYPE_BREEDER_HPP
