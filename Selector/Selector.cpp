dd949e917d08fb37ed3f82b3f8425eebc3fe52be
Gian <gac00056@students.stir.ac.uk>
2022-10-01 11:39:51 +0100

    Added initial Selector Class

---
+++ Selector/Selector.cpp
@@ -0,0 +1,8 @@
+//
+// Created by gian on 01/10/22.
+//
+
+#include "Selector.hpp"
+
+namespace GC {
+} // GC
---
+++ Selector/Selector.hpp
@@ -0,0 +1,16 @@
+//
+// Created by gian on 01/10/22.
+//
+
+#ifndef DISS_SIMPLEPROTOTYPE_SELECTOR_HPP
+#define DISS_SIMPLEPROTOTYPE_SELECTOR_HPP
+
+namespace GC {
+
+    class Selector {
+
+    };
+
+} // GC
+
+#endif //DISS_SIMPLEPROTOTYPE_SELECTOR_HPP
