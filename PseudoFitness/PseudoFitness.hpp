52cdfcf071a99f8fd24b9aedabff2f6ffe6dd7c7
Gian <gac00056@students.stir.ac.uk>
2022-10-09 13:51:06 +0100

    Implemented PseudoFitness

--- PseudoFitness/PseudoFitness.hpp
+++ PseudoFitness/PseudoFitness.hpp
@@ -4,10 +4,58 @@
 
 #ifndef DISS_SIMPLEPROTOTYPE_PSEUDOFITNESS_HPP
 #define DISS_SIMPLEPROTOTYPE_PSEUDOFITNESS_HPP
+#include "../Random/RandomChance.hpp"
+#include <sstream>
+#include <iomanip>
 
 namespace GC {
 
     class PseudoFitness {
+    public:
+        using FitnessScore = double;  //a compression ratio, compressedSize/originalSize
+        using Reliability = double;   //a value from 0 to 1, where 1 means 100% reliable
+        using Similarity = double;    //a value from 0 to 1, where 1 means identical
+
+    private:
+        FitnessScore fitnessScore;
+        Reliability reliability;
+
+        std::string to_string() const {
+            std::stringstream ss;
+            ss<<"{PseudoFitness:"<<(isActualFitness() ? "Actual" : "Approx")<<"="<<std::setprecision(2)<<fitnessScore;
+            if (!isActualFitness())
+                ss<<", Reliability="<<std::setprecision(2)<<reliability;
+            ss<<"}";
+
+            return ss.str();
+        }
+
+        Reliability getReliability() const {return reliability;}
+        FitnessScore getFitnessScore() const {return fitnessScore;}
+        bool isActualFitness() const {return reliability == 1.0;}
+
+
+        PseudoFitness():
+            fitnessScore(MAXFLOAT),
+            reliability(0){
+        }
+
+        PseudoFitness(const FitnessScore fs) :  //an actual fitness
+            fitnessScore(fs),
+            reliability(1.0) {
+        }
+
+        PseudoFitness(const FitnessScore approxFs, const Reliability r) : //an estimated fitness
+            fitnessScore(approxFs),
+            reliability(r) {
+        }
+
+        void setActualFitness(const FitnessScore fs) {
+            fitnessScore = fs;
+            reliability = 1;
+        }
+
+
 
     };
 
