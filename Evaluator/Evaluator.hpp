090c75b97b221b0fbb68d241b7e1a3593b490799
Gian <gac00056@students.stir.ac.uk>
2022-10-09 13:52:48 +0100

    Added files for Evaluator

---
+++ Evaluator/Evaluator.hpp
@@ -0,0 +1,16 @@
+//
+// Created by gian on 09/10/22.
+//
+
+#ifndef DISS_SIMPLEPROTOTYPE_EVALUATOR_HPP
+#define DISS_SIMPLEPROTOTYPE_EVALUATOR_HPP
+
+namespace GC {
+
+    class Evaluator {
+
+    };
+
+} // GC
+
+#endif //DISS_SIMPLEPROTOTYPE_EVALUATOR_HPP
