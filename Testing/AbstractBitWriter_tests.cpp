95b1fff82d12c783ef0adc965dd22c6ff33937ff
Gian <gac00056@students.stir.ac.uk>
2022-11-09 13:00:33 +0000

    Added test cases for consistency between BitCounter and BitVectorWriter

--- Testing/AbstractBitWriter_tests.cpp
+++ Testing/AbstractBitWriter_tests.cpp
@@ -66,7 +66,72 @@ namespace GC {
                 }
             }
         }
+    }
 
+    TEST_CASE("BitCounter remains consistent with VectorBitWriter") {
+        GIVEN("Writers is initially empty") {
+            BitCounter bitCounter;
+            VectorBitWriter vectorWriter;
+
+#define TO_BOTH_WRITERS(funcCall) bitCounter . funcCall; vectorWriter . funcCall
+
+            auto stillConsistent = [&bitCounter, &vectorWriter]()->bool {
+                return bitCounter.getCounterValue() == vectorWriter.getVector().size();
+            };
+
+            WHEN("Pushing a single value") {
+                TO_BOTH_WRITERS(pushBit(0));
+
+                THEN("They remain consistent") {
+                    CHECK(stillConsistent());
+                }
+            }
+
+            WHEN("Pushing multiple values") {
+                size_t amount = 108;
+                repeat(108, [&]() {
+                    TO_BOTH_WRITERS(pushBit(0));
+                });
+
+                THEN("They remain consistent") {
+                    CHECK(stillConsistent());
+                }
+            }
+
+            WHEN("Calling .writeAmountOfBits") {
+                size_t amountOfBits = GENERATE(1, 12);
+                THEN("They remain consistent") {
+                    TO_BOTH_WRITERS(writeAmountOfBits(6, amountOfBits));
+                    CHECK(stillConsistent());
+                }
+            }
+
+            WHEN("Calling writeUnary") {
+                size_t amountToEncode = GENERATE(1, 6, 36, 108);
+                THEN("Remain consistent") {
+                    TO_BOTH_WRITERS(writeUnary(amountToEncode));
+                    CHECK(stillConsistent());
+                }
+            }
+
+            WHEN("Calling writeSmallAmount (same as RiceEncoded)") {
+                size_t amountToEncode = GENERATE(1, 6, 36, 108);
+                THEN("Remain consistent") {
+                    TO_BOTH_WRITERS(writeSmallAmount(amountToEncode));
+                    CHECK(stillConsistent());
+                }
+            }
+
+            WHEN("Writing a vector") {
+                size_t lengthOfVector = GENERATE(0, 1, 6);
+                std::vector<bool> toWrite(true, lengthOfVector);
+
+                THEN("Remain consistent") {
+                    TO_BOTH_WRITERS(writeVector(toWrite));
+                    CHECK(stillConsistent());
+                }
+            }
+        }
     }
 }
 
